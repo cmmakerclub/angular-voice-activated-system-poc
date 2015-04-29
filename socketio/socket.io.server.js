79afc60f1a2e7ed82f98fe5ef11e1a17442216c7
Nat Weerawan <nat.wrw@gmail.com>
2015-04-29 17:38:56 +0700

    remove rfid code.

--- socketio/socket.io.server.js
+++ socketio/socket.io.server.js
@@ -3,47 +3,10 @@
     'use strict';
 
     var io = require('socket.io')(4000);
-    var rfid = require("./node-rfid/serial");
-
-
-    rfid.connect();
-
-
-    rfid.on("connect", function() {
-      console.log("connection"); 
-    });
-
-    rfid.on("data", function(data) {
-        console.log("DATA: ", data);
-        var name;
-        var object = { 
-            name: {},
-            cardId: data
-        }
-
-        if (data == "BDB27BCC") {
-            object['name'].th = "แมน";
-            object['name'].en = "Man";
-        }
-        else if (data == "A5B5522F") {
-            object['name'].th = "Nat";
-            object['name'].en = "Nat";
-        }
-        else {
-            object['name'].th = "none";
-            object['name'].en = "none";
-
-        }
-
-        io.sockets.emit('rfid-read', object);
-    });
 
     io.sockets.on('connection', function(socket) {
         console.info('client connected id:', socket.conn.id,
-                    ' from:', socket.handshake.headers.origin);
-
-
-
+                    ' from:', socket.handshake.headers.origin); 
         // var user;
 
         // socket.on('clear', function () {
